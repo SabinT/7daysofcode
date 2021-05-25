d932ae86e5b3d4ad2a4f80f6a32217047e464787
Sabin <sabintimalsena@gmail.com>
2021-05-24 20:24:52 -0700

    Change prompts

--- src/components/Prompts.ts
+++ src/components/Prompts.ts
@@ -1,14 +1,13 @@
 // key: YYYY-MM-DD
 // value: prompt
 const prompts: { [key: string]: string; }  = {
-    '2021-06-01': 'Earth',
+    '2021-06-01': 'Sun',
     '2021-06-02': 'Water',
-    '2021-06-03': 'Rock',
-    '2021-06-04': 'Trees',
-    '2021-06-05': 'Sky',
+    '2021-06-03': 'Leaf',
+    '2021-06-04': 'Sky',
+    '2021-06-05': 'Flower',
     '2021-06-06': 'Fire',
-    '2021-06-07': 'Wind',
-
+    '2021-06-07': 'Wind'
 };
 
 export function getPromptForDate(date: Date) {
